1a9ab8e3b6907aa5770ee6cafb533c7025a7201c
Andrew <chibisov.a@internet.ru>
2022-03-11 17:09:01 +0500

    init

---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="@/assets/logo.svg">
+      </a>
+
+      <a class="navbar-burger">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+
+        <a class="navbar-item">
+          Статьи
+        </a>
+
+        <a class="navbar-item">
+          Профиль
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Вход</strong>
+            </a>
+            <a class="button is-light">
+              Создать
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'MvcNavBar'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bulma/css/bulma.css'
+import 'bulma/js/bulma.js'
 
 createApp(App).use(store).use(router).mount('#app')
